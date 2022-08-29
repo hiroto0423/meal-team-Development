4c004879fb047e1e40fa8a964c38a22808012b36
emioiso <jijjjiijiijijiji@gmail.com>
2022-08-29 21:59:58 +0900

    　料理詳細のhtmlとcssを記述しました

--- src/resources/views/meals/show.blade.php
+++ src/resources/views/meals/show.blade.php
@@ -1,60 +1,48 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="/css/style.css" >
-    </head>
-    <body>
-    　<div class="header">
-    　　<div class="header-logo">男の自炊アプリ</div>
-    　　<div class="header-list">
-　　      <button><a href="/register">レシピ登録</a></button>
-　　      <button><a href="/meals">料理一覧</a></button>
-　　      <button><a href="/categoly">カテゴリー</a></button>
-    　　</div>
-    　</div>
-    　<div class="main">
-    　  <div class="meal-detail">
-          <button><a href="/">編集</a></button>
-        　<form action='/'id='' method="post" style="display:inline">
-            @csrf
-            @method('DELETE')
-            <button type="submit">消去</button> 
-        　</form>
-    　     <div class="meal-name">
-           <h2>料理名</h2>
-           <p></p>
-          </div>
-            
-          <div class="meal-Ingredients_Memo">
-            <h2>材料メモ</h2>
-          </div>
-            
-            <div class="meal-way">
-              <h2>作り方</h2>
+<html lang="{{ str_replace('', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link rel="stylesheet" href="/css/style.css" >
+</head>
+<body>
+    <div class="header">
+        <div class="header-logo">男の自炊アプリ</div>
+        <div class="header-list">
+            <button><a href="/register">レシピ登録</a></button>
+            <button><a href="/meals">料理一覧</a></button>
+            <button><a href="/categoly">カテゴリー</a></button>
+        </div>
+    </div>
+
+    <div class="meal-wrapper">
+        <div class="name-block">
+            <p class="meal-name">チキンステーキ</p>
+            <div class="infomation">
+                <div class="cost orange_column">
+                    <p>コスト</p>
+                    <p id="">500円</p>
+                </div>
+                <div class="satiety orange_column">
+                    <p>満腹度</p>
+                    <p id="">がっつり</p>
+                </div>
+                <div class="diffyculty orange_column">
+                    <p>難易度</p>
+                    <p id="">レベル2</p>
+                </div>
             </div>
-    
-            <div class="meal-cost">
-              <h2>コスト</h2>
-            </div>  
-            
-            <div class="meal-categoly">
-              <h2>カテゴリー</h2>
+        </div>
+        <div class="memo">
+            <img class="ao" src="/image/chicken.png" alt="" width="440" height="440">
+            <div class="memonoNakami">
+                <h2>材料メモ</h2>
+                <p id="">ここに材料メモが入る</p>
+                <h2>作り方</h2>
+                <p id="">ここに作り方が入る</p>
             </div>
-            
-            <div clas="meal-diffyculty">
-              <h2>難易度</h2>
-            </div>
-            
-            <div class="meal-satiety">
-              <h2>満腹度</h2>
-            </div>  
-    　  </div>
-      </div>
-      <div class="footer">
-          
-      </div>
-    </body>
+        </div>
+    </div>
+</body>
 </html>
