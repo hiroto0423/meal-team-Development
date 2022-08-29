6719047e180d105cebcde76fbf338fe661799274
Garbage-soft <wake.awakens3@gmail.com>
2022-08-29 20:17:37 +0900

    機能修正：フォームの文字列型データの文字数制限のバリデーション処理を追加しました

--- src/public/js/front_form_validation.js
+++ src/public/js/front_form_validation.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-  console.log('loading validation file')
+
   // 画面オブジェクト
   const btnSubmit        = document.getElementById('meal_create');
   const inputMealName    = document.getElementById('inputMealName');
@@ -14,17 +14,30 @@ window.onload = function() {
     const REGEX = /[0-9]/
     // フォームの入力値チェック
 
-    // 画像添付に関するバリデーションは料理登録画面修正時にまた別途行います。
+      // 画像添付に関するバリデーションは料理登録画面修正時にまた別途行います。
 
     if(inputMealName.value == "") {
       message.push("料理名が未入力です。");
     }
+
+    if(inputMealName.value.length > 50) {
+      message.push("料理名は50文字以内で記入してください。 現在の文字数:" + inputMealName.value.length + "文字")
+    }
+
     if(inputIngredients.value == "") {
       message.push("材料メモが未入力です。")
     }
+    if(inputIngredients.value.length > 200) {
+      message.push("材料メモは200文字以内で記入してください。 現在の文字数:" + inputIngredients.value.length + "文字")
+    }
+
     if(inputWay.value == "") {
       message.push("作り方が未入力です。")
     }
+    if(inputWay.value.length > 500) {
+      message.push("作り方は500文字以内で記入してください。 現在の文字数:" + inputWay.value.length + "文字")
+    }
+
     if(inputCost.value == "") {
       message.push("コストが未入力です。")
     }
@@ -33,7 +46,6 @@ window.onload = function() {
     }
 
     if(message.length > 0) {
-      console.log(message.length);
       alert(message.join('\n'));   // バリデーションメッセージの表示
       return;
     }
