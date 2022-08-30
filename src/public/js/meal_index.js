48a9f701ee3ad2625b7f5dbcba1d5d13b1403121
Garbage-soft <wake.awakens3@gmail.com>
2022-08-30 22:13:01 +0900

    料理一覧画面のマークアップ、JSでのHTML生成機能の追加（途中）

--- src/public/js/meal_index.js
+++ src/public/js/meal_index.js
@@ -2,30 +2,68 @@ const url = 'http://localhost:9000/';
 const endpoint_mealindex ='api/mealindex';
 
 window.onload = function(){
-  console.log('open_index');
+//   console.log('open_index');
 
-  fetch(url+endpoint_mealindex, {
-    method: 'GET',
-}).then((response) => {
-    if(!response.ok) {
-        console.log('Create error!');
-        throw new Error('error');
-    } ;
-    console.log('response ok');
-    return response.json();
-}).then((response)  => {
-   for(var i = 0 ; i<response.length ; i++){
-    // console.log(response[i].id);
-    // console.log(response[i].name);
-    var list = document.createElement('a');
-    list.setAttribute("id", response[i].id);
-    list.setAttribute("href", url + 'meals/' + response[i].id);
-    list.setAttribute("class", 'meal-name');
-    list.innerHTML = response[i].name;
-    var parent = document.getElementById('addlistBox');
-    parent.appendChild(list);
-   };
-}).catch((response) => {
-    console.log(error);
-});
-};
+//   fetch(url+endpoint_mealindex, {
+//     method: 'GET',
+// }).then((response) => {
+//     if(!response.ok) {
+//         console.log('Create error!');
+//         throw new Error('error');
+//     } ;
+//     console.log('response ok');
+//     return response.json();
+// }).then((response)  => {
+
+  // 料理一覧を表示させる繰り返し処理
+
+  for(var i = 0 ; i < 10 ; i++){
+
+    // 必要なHTML要素を定義
+    var itemWrapper  = document.createElement('div');itemWrapper.setAttribute('class', 'item-wrapper');
+    itemWrapper.setAttribute('id', 'item_wrapper-' + i);
+
+    var imageWrapper = document.createElement('div');imageWrapper.setAttribute('class', 'image-wrapper');
+    imageWrapper.setAttribute('id', 'image_wrapper-' + i);
+
+    var tmpImage = document.createElement('div');
+    tmpImage.setAttribute('class', 'tem-image');
+    tmpImage.setAttribute('id', 'tmp_image-' + i);
+
+    var mealNameBlock = document.createElement('div');mealNameBlock.setAttribute('class', 'meal-name-block');
+    mealNameBlock.setAttribute('id', 'meal_name_block-' + i);
+
+    var mealUrl = document.createElement('a');
+    mealUrl.setAttribute('href', url + 'meals/' + i);
+    mealUrl.setAttribute('id', 'meal_url');
+    mealUrl.innerHTML = '料理-' + i
+
+    // HTML要素を生成
+
+    document.getElementById('addListBox').appendChild(itemWrapper);
+    document.getElementById('item_wrapper-' + i).appendChild(imageWrapper);
+    document.getElementById('item_wrapper-' + i).appendChild(mealNameBlock);
+    document.getElementById('image_wrapper-' + i).appendChild(tmpImage);
+    document.getElementById('meal_name_block-' + i).appendChild(mealUrl);
+  }
+}
+
+
+
+
+    // var list = document.createElement('a');
+
+    // // list.setAttribute("id", 'meal_url-' + response[i].id);
+    // list.setAttribute("id", 'meal_url');
+    // list.setAttribute("href", url + 'meals/' + response[i].id);
+    // list.setAttribute("class", 'meal-name');
+
+    // list.innerHTML = response[i].name;
+
+    // var parent = document.getElementById('addlistBox');
+    // parent.appendChild(list);
+  // };
+// }).catch((response) => {
+//     console.log(error);
+// });
+// };
--- src/resources/views/meals/list.blade.php
+++ src/resources/views/meals/list.blade.php
@@ -7,22 +7,29 @@
         <link rel="stylesheet" href="/css/style.css" >
     </head>
     <body>
-    　<div class="header">
-    　　<div class="header-logo">男の自炊アプリ</div>
-    　　<div class="header-list">
-　　      <button><a href="/register">レシピ登録</a></button>
-　　      <button><a href="/meals">料理一覧</a></button>
-　　      <button><a href="/categoly">カテゴリー</a></button>
-    　　</div>
-    　</div>
-    　<div class="main">
-    　   <h1>レシピ一覧</h1>
-    　  <div id="addlistBox"></div>
+    <div class="header">
+      <div class="header-logo">男の自炊アプリ</div>
+          <div class="header-list">
+            <button><a href="/register">レシピ登録</a></button>
+            <button><a href="/meals">料理一覧</a></button>
+            <button><a href="/categoly">カテゴリー</a></button>
       </div>
-      
-      <div class="footer">
-          
+    </div>
+    <div class="main">
+      <h1>レシピ一覧</h1>
+      <div class="index-container" id="addListBox">
+
+        <div class="item-wrapper">
+          <div class="image-wrapper">
+            <div class="tmp-image">ここに画像が入る</div>
+          </div>
+          <div class="meal-name-block">
+            <a href="#" class="tmp-name" id="meal_url">料理名</a>
+          </div>
+        </div>
+
       </div>
+    </div>
       <script src="js/meal_index.js"></script>
     </body>
 </html>
