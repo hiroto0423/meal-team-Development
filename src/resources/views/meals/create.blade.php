df0d8c62220d80a0d7681ac5454dc4055684ddad
Garbage-soft <wake.awakens3@gmail.com>
2022-08-30 17:54:01 +0900

    修正：指摘のあったHTML要素の修正

--- src/resources/views/meals/create.blade.php
+++ src/resources/views/meals/create.blade.php
@@ -7,56 +7,50 @@
         <link rel="stylesheet" href="/css/style.css" >
     </head>
     <body>
-    　<div class="header">
-    　　<div class="header-logo">男の自炊アプリ</div>
-    　　<div class="header-list">
-　　      <button><a href="/register">レシピ登録</a></button>
-　　      <button><a href="/meals">料理一覧</a></button>
-　　      <button><a href="/categoly">カテゴリー</a></button>
-    　　</div>
-    　</div>
+      <div class="header">
+        <div class="header-logo">男の自炊アプリ</div>
+      <div class="header-list">
+          <button><a href="/register">レシピ登録</a></button>
+          <button><a href="/meals">料理一覧</a></button>
+          <button><a href="/categoly">カテゴリー</a></button>
+      </div>
+    </div>
       <div class="main">
         <div class="meal-register">
           <form action="/register" method="POST" id='form' name="meal_form">
             <h1>料理登録</h1>
 
-            <h2>写真を登録する</h2>
-            <div style="text-align:center">
-              <form action="index.php" method="post" enctype="multipart/form-data" >
-              <label>
-                <input class="sentaku" type="file" name="image" accept="image/*" >写真を選択
-              </label>
-            </div>
+            <!-- 画像投稿機能は後ほど実装します -->
 
             <div class="meal-name">
-              <h2>料理名</h2>
+              <label>料理名</label>
               <input type="text" placeholder="オムライス" name="meal_name">
             </div>
 
             <div class="meal-Ingredients_Memo">
-              <h2>材料メモ</h2>
+              <label>材料メモ</label>
               <textarea type="text" placeholder="卵二個・牛乳" name="meal_ingredients"></textarea>
             </div>
 
             <div class="meal-way">
-              <h2>作り方</h2>
-              <input type="text" placeholder="卵を割って～～～" name="meal_way">
+              <label>作り方</label>
+              <textarea placeholder="卵を割って～～～" name="meal_way"></textarea>
             </div>
 
             <div class="meal-cost">
-              <h2>コスト</h2>
+              <label>コスト</label>
               <input type="number" placeholder="500" name="meal_cost">
             </div>
 
-            <div clas="meal-diffyculty">
-              <h2>難易度</h2>
+            <div class="meal-diffyculty">
+              <label>難易度</label>
               <input type="radio"name="難易度"value=1 checked> レベル1
               <input type="radio"name="難易度"value=2> レベル２
               <input type="radio"name="難易度"value=3> レベル3
             </div>
 
             <div class="meal-satiety">
-              <h2>満腹度</h2>
+              <label>満腹度</label>
               <input type="radio"name="満足度"value=1 checked> ちょっと食べたい
               <input type="radio"name="満足度"value=2> ちょうどいい
               <input type="radio"name="満足度"value=3> がっつり
