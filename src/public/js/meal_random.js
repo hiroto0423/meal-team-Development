dc7ec754769486e2da06c9a137c9ff1a6b30c7e4
root <root@DESKTOP-64Q5VAN.localdomain>
2022-09-02 22:58:38 +0900

    料理ランダム表示機能の実装

---
+++ src/public/js/meal_random.js
@@ -0,0 +1,31 @@
+// ルートURLの取得
+var root_url = location.protocol + '//' + location.hostname;
+if(!location.port == '') {
+  console.log('set url');
+  root_url = root_url + ':' + location.port
+}
+
+const btnSubmit = document.getElementById('random_button');
+//mealsテーブルからランダムに一つデータを取得
+function CallRandomMealGetApi(){
+  fetch(root_url + '/api/random_meal', {
+    method: 'GET',
+  }).then((response) => {
+    if(!response.ok) {
+        console.log('Get error!');
+        throw new Error('error');
+    } ;
+    console.log('Get');
+    return response.json();
+  }).then((response)  => {
+    console.log(response[0].name);
+    var meal__name = document.getElementById('meal_name');
+    meal_name.innerHTML = response[0].name;
+    meal_name.setAttribute('href', root_url + '/meals/' + response[0].id);
+  }).catch((error) => {
+    console.log(error);
+    console.log('料理を登録してください');
+    window.location.href = root_url + '/register' ;
+  });
+}
+btnSubmit.addEventListener('click',CallRandomMealGetApi , false)
